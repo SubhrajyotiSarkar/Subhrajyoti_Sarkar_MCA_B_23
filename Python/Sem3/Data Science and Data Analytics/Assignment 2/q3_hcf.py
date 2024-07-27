af721139b933bb52f6af3853ffebc2943f0cb98c
Subhrajyoti Sarkar <109728599+SubhrajyotiSarkar@users.noreply.github.com>
2024-07-27 12:02:03 +0530

    Update q3_hcf.py

--- Python/Sem3/Data Science and Data Analytics/Assignment 2/q3_hcf.py
+++ Python/Sem3/Data Science and Data Analytics/Assignment 2/q3_hcf.py
@@ -1,13 +1,13 @@
-def hcf(a, b):
-    if a % b:
-        return hcf(a, a % b)
-    else:
-        return b
-
-
-a = int(input("Enter First Number: "))
-b = int(input("Enter Second Number: "))
-if a < b:
-    a, b = b, a
-
-print(f"HCF of {a} and {b} is: {hcf(a,b)}")
+def hcf(x, y):  
+    if x > y:  
+            smaller = y  
+    else:  
+            smaller = x  
+    for i in range(1,smaller + 1):  
+            if((x % i == 0) and (y % i == 0)):  
+               hcf = i  
+    return hcf  
+  
+num1 =int(input("Enter the Fisrst number: "))   
+num2 =int(input("Enter the Second number: "))  
+print("The H.C.M of", num1,"and", num2,"is", hcf(num1, num2))
