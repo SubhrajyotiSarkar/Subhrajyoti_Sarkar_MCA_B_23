c8e91f71c621d73ba125beeddc2b04957b8a4a34
Subhrajyoti Sarkar <109728599+SubhrajyotiSarkar@users.noreply.github.com>
2024-07-28 19:20:46 +0530

    commit

--- Python/Sem3/Data Science and Data Analytics/Assignment 3/q9_pattern.py
+++ Python/Sem3/Data Science and Data Analytics/Assignment 3/q9_pattern.py
@@ -1,4 +1,4 @@
-#    *
+#    .
 #   / \
 #  /   \
 # /_____\
@@ -7,7 +7,7 @@ n = int(input("Enter Number of Rows: "))
 for i in range(n):
     spaces = " " * (n - i - 1)
     if i == 0:
-        print(spaces + "*")
+        print(spaces + ".")
     elif i == n - 1:
         print("/" + "_" * (2 * i - 1) + "\\")
     else:
